8a704768858b0a010057ff5487bbf6c2aefa6b2b
Ulan-2507 <72508939+Ulan-2507@users.noreply.github.com>
2021-04-19 12:47:26 +0600

    Add files via upload

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import 'antd/dist/antd.css';
+import { Spin } from 'antd';
+import { Alert } from 'antd';
 import Movie from '../movie';
-const MOVIES_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8cb439222da4a8901afb40ed93e0947f&page=1';
 
+const MOVIES_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8cb439222da4a8901afb40ed93e0947f&page=1';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=8cb439222da4a8901afb40ed93e0947f&query=';
 
 
 function App () {
     const [ movies, setMovies ] = useState([]);
     const [ searchTerm, setSearchTerm ] = useState('');
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(false);
 
-    const getMovies = async (API) => {
-        const moviesResp = await fetch (API);
-        const data = await moviesResp.json();
-        setMovies(data.results);
+    const getMovies = (API) => {
+        fetch(API)
+            .then(response =>{ 
+                if(response.status === 200) { 
+                    return response.json()
+                } else {
+                    throw new Error()
+                }
+            })
+            .then(data => {
+                setMovies(data.results)
+                setLoading(false)
+            })
+            .catch(err => onError(err));
     }
+
+    const onError = () => {
+        setLoading(false);
+        setError(true);
+    }
+
     useEffect(() => {
         getMovies(MOVIES_API);
     }, []);
@@ -29,6 +50,14 @@ function App () {
     const handleOnChange = (e) => {
         setSearchTerm(e.target.value);
     }
+
+    const errorMessage = error ? <Alert
+                            message="ошибка"
+                            description="попробуйте еще раз"
+                            type="error"
+                        /> : null;
+    const antIcon = loading ? <Spin size='large'/> : null;
+    const moviesData = movies.length > 0 ? movies.map(movie => <Movie key={movie.id} {...movie}/>) : null;
     return (
         <>
             <header className='header'>
@@ -45,10 +74,11 @@ function App () {
                         onChange={handleOnChange}/>
                 </form>
             </header>
-            <div className='movies-container'>
-                {movies.length > 0 && movies.map(movie => 
-                <Movie key={movie.id} {...movie}/>)}
-            </div>
+            <section className='movies-container'>
+                {errorMessage}
+                {antIcon}
+                {moviesData}
+            </section>
         </>
         
     )
--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import format from 'date-fns/format'
 import './movie.css';
+import defaultPoster from './default-poster.jpg';
+
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+
 // const GENRE_API ='https://api.themoviedb.org/3/genre/movie/list?api_key=8cb439222da4a8901afb40ed93e0947f';
 const textLimit = (text) => {
      const words = text.split(' ');
@@ -20,18 +23,21 @@ const textLimit = (text) => {
 }
 
 const Movie = ({ title, poster_path, overview, vote_average, release_date }) => {
+    console.log(release_date)
     const text = textLimit(overview) ;
+    const poster = poster_path ? IMG_API + poster_path : defaultPoster;
+    const release = release_date ? format(new Date(release_date),'MMMM d, yyyy') : 'no release date';
     return (
         <div className='movie'>
              <div className='movie__poster'>
-                <img src={ IMG_API + poster_path } alt={ title }/>
+                <img src={ poster } alt={ title }/>
             </div>
             <div className='movie__info'>
                 <div className='movie__header'>
                     <h5>{ title }</h5>
                     <span>{ vote_average }</span>
                 </div>
-                <div className='movie__release-date'>{ format(new Date(release_date),'MMMM d, yyyy') }</div>
+                <div className='movie__release-date'>{ release }</div>
                 <div className='movie__genre'>
                     <a href='#'>Action</a>
                     <a href='#'>Drama</a>
